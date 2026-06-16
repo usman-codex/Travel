bae214605f066b5c645ce277799c8fab72f3d1c9
USMAN AHMAD <usmaneng220@gmail.com>
2026-06-16 12:51:23 +0500

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
-
+import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer"; 
 import React from "react"; 
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Travel Operations | Best Travel Agency",
+  description: "Explore the world with our exciting range of tours and visa services.",
+  icons: {
+    icon: "/icon.png", 
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
